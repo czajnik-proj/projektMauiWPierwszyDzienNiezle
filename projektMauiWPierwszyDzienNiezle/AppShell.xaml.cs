d458ac52c05e48e147df434c3113738ec1519ac2
andrzejczak.igor.te_ <andrzejczak.igor.te_@30NKomp16>
2025-09-08 11:54:49 +0200

    Dodaj pliki projektów.

---
+++ projektMauiWPierwszyDzienNiezle/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace projektMauiWPierwszyDzienNiezle
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ projektMauiWPierwszyDzienNiezle/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace projektMauiWPierwszyDzienNiezle
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
