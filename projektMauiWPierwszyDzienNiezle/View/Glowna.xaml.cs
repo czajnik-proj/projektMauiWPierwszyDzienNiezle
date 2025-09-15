0413b58fac04b11f4729e0dc6e585398d990572b
dave <libuszowski.dawid.te@edu.zstjaslo.pl>
2025-09-15 11:48:10 +0200

    viewmodel

--- projektMauiWPierwszyDzienNiezle/View/Glowna.xaml.cs
+++ projektMauiWPierwszyDzienNiezle/View/Glowna.xaml.cs
@@ -5,5 +5,6 @@ public partial class Glowna : ContentPage
 	public Glowna()
 	{
 		InitializeComponent();
+		BindingContext = new ViewModel.MealViewModel();
 	}
 }
---
+++ projektMauiWPierwszyDzienNiezle/ViewModel/MealViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using projektMauiWPierwszyDzienNiezle.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektMauiWPierwszyDzienNiezle.ViewModel
+{
+    internal partial class MealViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<Meal> _MealCollection;
+        public MealViewModel() {}
+
+        public void AddMeal(Meal MealItem)
+        {
+            _MealCollection.Append(MealItem);
+        }
+        public void DeleteMeal(Meal MealItem)
+        {
+            int removeIndex = _MealCollection.IndexOf(MealItem);
+            if (removeIndex != -1)
+            {
+                _MealCollection.RemoveAt(removeIndex);
+            }
+        }
+        public void EditMeal(Meal MealItem, string name, int kcal, int servings)
+        {
+            Meal editedMeal = new Meal(name, kcal, servings);
+            int editIndex = _MealCollection.IndexOf(MealItem);
+            if (editIndex != -1)
+            {
+                _MealCollection[editIndex] = MealItem;
+            }
+        }
+
+    }
+}
