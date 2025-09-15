50b3618a03f18146e8601154d3ad85c2360604b7
firlit.michal.te_edu <firlit.michal.te@edu.zstjaslo.pl>
2025-09-15 11:49:14 +0200

    master

--- projektMauiWPierwszyDzienNiezle/Model/Meal.cs
+++ projektMauiWPierwszyDzienNiezle/Model/Meal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace projektMauiWPierwszyDzienNiezle.Model
 {
-    internal class Meal
+    internal partial class Meal
     {
         public string Name { get; set; }
         public int Kcal {  get; set; }
